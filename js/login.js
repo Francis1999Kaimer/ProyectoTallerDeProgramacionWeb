06fc6008a8a8cb1b6747cfade73ca3143e3c9be3
Francis1999Kaimer <49914728+Francis1999Kaimer@users.noreply.github.com>
2024-05-24 13:02:52 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,30 +1,22 @@
-
-
-// Función para validar el usuario y la contraseña
 function validarUsuario(usuario, clave) {
-    // Comprobar si el usuario y la clave coinciden con los valores esperados
+
     if (usuario === "admin" && clave === "123456") {
-        // Si coinciden, devolver verdadero
-        return true;
+        return true;   
     } else {
-        // Si no coinciden, devolver falso
-        return false;
+        return false; 
     }
 }
 
-// Función para manejar el inicio de sesión
+
 function iniciarSesion() {
-    // Obtener los valores del usuario y la contraseña del formulario de inicio de sesión
+
     var usuario = document.getElementById("usuario").value;
     var clave = document.getElementById("clave").value;
-
-    // Validar el usuario y la contraseña
+    
     if (validarUsuario(usuario, clave)) {
-        // Si son válidos, mostrar un mensaje de bienvenida
         window.location.href = "ingreso.html";
-        alert("¡Bienvenido, " + usuario + "!");
+        alert("¡Bienvenido, " + usuario + "!"); 
     } else {
-        // Si no son válidos, mostrar un mensaje de error
         alert("Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.");
-    }
+    } 
 }
